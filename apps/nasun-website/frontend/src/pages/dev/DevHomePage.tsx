b9d92a3adb301253ac60104e6e0fb03bf494a3fe
narunice <narunice@users.noreply.github.com>
2026-04-16 17:41:35 +0900

    nasun home page renewal roll back point

--- apps/nasun-website/frontend/src/pages/dev/DevHomePage.tsx
+++ apps/nasun-website/frontend/src/pages/dev/DevHomePage.tsx
@@ -1,11 +1,15 @@
 import { ScrollSnapContainer } from "@/components/layout/ScrollSnapContainer";
 import { ScrollSnapSection } from "@/components/layout/ScrollSnapSection";
 import TriptychSection from "@/sections/home/TriptychSection";
+import Hero2026Section from "@/sections/home/2026HeroSection";
 
 export default function DevHomePage() {
   return (
     <div className="bg-nasun-black">
       <ScrollSnapContainer>
+        <ScrollSnapSection>
+          <Hero2026Section />
+        </ScrollSnapSection>
         <ScrollSnapSection>
           <TriptychSection />
         </ScrollSnapSection>
